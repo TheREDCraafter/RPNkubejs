3a02565be04e556738c976394202c9e6b80d300c
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-06 19:45:34 +0100

    rererere fix...

--- server_scripts/src/rathaus.js
+++ server_scripts/src/rathaus.js
@@ -9,7 +9,7 @@ ServerEvents.commandRegistry(event => {
                     context.source.player.runCommandSilent("playsound minecraft:block.bell.use master @s[tag=!rpn.ring] ~ ~ ~ 1 1");
                     context.source.player.getServer().players.forEach(element => {
                         element.runCommandSilent("playsound minecraft:entity.experience_orb.pickup master @s[tag=rpn.ring] ~ ~ ~ 1 1");
-                        element.runCommandSilent(`tellraw @s[tag=rpn.ring] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":true,"hoverEvent":{"action":"show_text","value":[{"text":"${context.source.player.getString()}"}]}}`);
+                        element.runCommandSilent(`tellraw @s[tag=rpn.ring] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":true,"hoverEvent":{"action":"show_text","value":[{"text":"${context.source.player.getName().getString()}"}]}}`);
                     });
                     return 1;
                 })
