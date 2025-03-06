60214bde3a888ad43f7e3a8842dfbd372f095fcf
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-06 19:21:00 +0100

    chore typo

--- server_scripts/src/rathaus.js
+++ server_scripts/src/rathaus.js
@@ -7,7 +7,7 @@ ServerEvents.commandRegistry(event => {
             .requires(source => source.hasPermission(2))
             .then(Commands.literal("ring")
                 .executes(context => {
-                    context.source.player.runCommandSilent("playsound minecraft:block.bell.use master @s[tag=rpn.ring] ~ ~ ~ 1 1");
+                    context.source.player.runCommandSilent("playsound minecraft:block.bell.use master @s[tag=!rpn.ring] ~ ~ ~ 1 1");
                     context.source.player.getServer().players.forEach(element => {
                         element.runCommandSilent("playsound minecraft:entity.experience_orb.pickup master @s[tag=rpn.ring] ~ ~ ~ 1 1");
                         element.runCommandSilent(`tellraw @s[tag=rpn.ring] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":"true"}`);
