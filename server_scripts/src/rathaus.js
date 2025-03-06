3f3779e4ad8cfb31b320c5cc9d863b29e4c78b8d
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-06 19:31:53 +0100

    chore formatting + typo

--- server_scripts/src/rathaus.js
+++ server_scripts/src/rathaus.js
@@ -1,5 +1,4 @@
 ServerEvents.commandRegistry(event => {
-
     const { commands: Commands, arguments: Arguments } = event;
 
     event.register(
@@ -10,7 +9,7 @@ ServerEvents.commandRegistry(event => {
                     context.source.player.runCommandSilent("playsound minecraft:block.bell.use master @s[tag=!rpn.ring] ~ ~ ~ 1 1");
                     context.source.player.getServer().players.forEach(element => {
                         element.runCommandSilent("playsound minecraft:entity.experience_orb.pickup master @s[tag=rpn.ring] ~ ~ ~ 1 1");
-                        element.runCommandSilent(`tellraw @s[tag=rpn.ping] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":true,"hoverEvent":{"action":"show_text","value":[{"text":"${context.source.player.getName()}"}]}}`);
+                        element.runCommandSilent(`tellraw @s[tag=rpn.ring] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":true,"hoverEvent":{"action":"show_text","value":[{"text":"${context.source.player.getName()}"}]}}`);
                     });
                     return 1;
                 })
