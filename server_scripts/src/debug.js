8ea5b5408a6edcb90b9c2026d417c37dc7ba339b
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-06 18:35:34 +0100

    changed debug to rpndbg to avoid conflict with std debug

--- server_scripts/src/debug.js
+++ server_scripts/src/debug.js
@@ -2,15 +2,22 @@ ServerEvents.commandRegistry(event => {
 
     const { commands: Commands, arguments: Arguments } = event;
 
-    event.register(Commands.literal("debug")
+    event.register(Commands.literal("rpndbg")
         .requires(source => source.hasPermission(2))
-        .then(Commands.literal("perso")
+        .then(Commands.literal("forceperso")
             .executes(context => {
                 const player = context.source.player;
                 player.runCommandSilent("tag @s remove rpn.perso");
                 player.runCommandSilent("perso @s");
                 return 1;
             })
-    ));
-
+        )
+        .then(Commands.literal("unperso")
+            .executes(context => {
+                const player = context.source.player;
+                player.runCommandSilent("tag @s remove rpn.perso");
+                return 1;
+            })
+        )
+    );
 });
