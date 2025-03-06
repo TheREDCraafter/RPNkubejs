eae653708f9629f0c11ea59a16407251f3abacc7
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-06 09:39:41 +0100

    Mooore debug

--- server_scripts/src/perso.js
+++ server_scripts/src/perso.js
@@ -93,6 +93,7 @@ function givePersonalausweis(player, data) {
     player.tell("Getting NBT");
     book.getNbt();
     player.tell(`Putting book into barrel with command 'execute in minecraft:overworld run item replace block ${copyx} ${copyy} ${copyz} container.0 with ${book.getId()}${book.getNbt()}'`);
+    console.log(`Putting book into barrel with command 'execute in minecraft:overworld run item replace block ${copyx} ${copyy} ${copyz} container.0 with ${book.getId()}${book.getNbt()}'`)
     player.runCommand(`execute in minecraft:overworld run item replace block ${copyx} ${copyy} ${copyz} container.0 with ${book.getId()}${book.getNbt()}`);
 
     if (player.getTags().contains("rpn.continuescene")){
