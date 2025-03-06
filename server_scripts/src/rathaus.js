ed21b0e5cdf64b45a592ce7072d49e5e81990605
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-06 19:35:10 +0100

    fixed wrong name format in /rathaus ring

--- server_scripts/src/rathaus.js
+++ server_scripts/src/rathaus.js
@@ -9,7 +9,7 @@ ServerEvents.commandRegistry(event => {
                     context.source.player.runCommandSilent("playsound minecraft:block.bell.use master @s[tag=!rpn.ring] ~ ~ ~ 1 1");
                     context.source.player.getServer().players.forEach(element => {
                         element.runCommandSilent("playsound minecraft:entity.experience_orb.pickup master @s[tag=rpn.ring] ~ ~ ~ 1 1");
-                        element.runCommandSilent(`tellraw @s[tag=rpn.ring] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":true,"hoverEvent":{"action":"show_text","value":[{"text":"${context.source.player.getName()}"}]}}`);
+                        element.runCommandSilent(`tellraw @s[tag=rpn.ring] {"text":"Im Rathaus wurde geklingelt!","color":"green","bold":true,"hoverEvent":{"action":"show_text","value":[{"text":"${context.source.player.getDisplayName()}"}]}}`);
                     });
                     return 1;
                 })
