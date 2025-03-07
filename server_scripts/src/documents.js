bdcc866eafce4ffebf29f973257b03d3c4259f9e
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-07 12:58:13 +0100

    quick7

--- server_scripts/src/documents.js
+++ server_scripts/src/documents.js
@@ -219,7 +219,7 @@ ServerEvents.commandRegistry(event => {
                         player.getMainHandItem().setCount(0);
 
                         player.tell(pages.toString().replace("\n", "\\n"));
-                        player.tell(`"pages": ${pages.toString().replace("\n", "\\n")}`);
+                        player.tell(`"pages": ${pages.toString()}`);
 
                         player.runCommand(`give @s minecraft:written_book{"title": "[${stamp}] ${name}", "author": "Rathaus [IC]", "display": {"Lore": ["{\"text\":\"[${stamp}] | Infinity City\",\"color\":\"dark_purple\",\"italic\":false}"]}, "pages": ${pages.toString().replace("\n", "\\n")}}`);
 
