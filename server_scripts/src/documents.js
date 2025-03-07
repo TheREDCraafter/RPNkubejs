7fe700758ea7118058f77696bcf974b38d9d1b4d
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-07 11:50:04 +0100

    maybe this time?

--- server_scripts/src/documents.js
+++ server_scripts/src/documents.js
@@ -223,7 +223,7 @@ ServerEvents.commandRegistry(event => {
                             title: `[${stamp}] ${name}`,
                             author: `Rathaus [IC]`,
                             display:{Lore:[`{"text":"[${stamp}] | Infinity City","color":"dark_purple","italic":false}`]},
-                            pages: pages.toString()
+                            pages: pages.toString().replace(/\n/, "\\n")
                         })
 
                         console.log("Newer NBT: \n\n" + signedBook.nbt);
