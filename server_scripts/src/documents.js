086c52e9b51f6dee885aa011482c0cf5a6aec4f3
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-07 13:11:03 +0100

    9

--- server_scripts/src/documents.js
+++ server_scripts/src/documents.js
@@ -221,7 +221,8 @@ ServerEvents.commandRegistry(event => {
                         player.tell(pages.toString().replace(/\n/g, "\\n"));
                         player.tell(`"pages": ${pages.toString()}`);
 
-                        player.runCommand(`give @s minecraft:written_book{title: "[${stamp}] ${name}", author: "Rathaus [IC]", display: {Lore:['{"text":"[${stamp}] | Infinity City","color":"dark_purple","italic":false}']}, pages: ${pages.toString().replace(/\n/g, "\\n")}}`);
+                        player.runCommand(`give @s minecraft:written_book{title: "[${stamp}] ${name}", author: "Rathaus [IC]", display: {Lore:['{"text":"[${stamp}] | Infinity City","color":"dark_purple","italic":false}']}, pages: ${pages.toString().replace("\n", "\\n")}}`);
+                        console.log(`give @s minecraft:written_book{title: "[${stamp}] ${name}", author: "Rathaus [IC]", display: {Lore:['{"text":"[${stamp}] | Infinity City","color":"dark_purple","italic":false}']}, pages: ${pages.toString().replace("\n", "\\n")}}`)
 
                         return 1;
                     })
