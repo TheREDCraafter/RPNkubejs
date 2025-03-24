0c80f228802980f27bbd3ec2313ed7419a40dc34
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-24 20:08:04 +0100

    Custom Block Test 1

---
+++ startup_scripts/src/customblocks.js
@@ -0,0 +1,32 @@
+const { $BlockEntityJS } = require("packages/dev/latvian/mods/kubejs/block/entity/$BlockEntityJS");
+const { $ItemStack } = require("packages/net/minecraft/world/item/$ItemStack");
+
+function checkWoodsawRecipe(_inventory) {
+    // ONLY FOR INTELLISENSE, REMOVE LATER
+    const entity = new $BlockEntityJS();
+    const inventory = entity.inventory;
+    // UNTIL HERE
+    
+    inventory.getAllItems().forEach(_item => { // AGAIN HERE
+        const item = new $ItemStack();
+        if (item.getId() === "minecraft:oak_log") {
+            if (item.getCount() >= 10) {
+                inventory.extractItem(inventory.find(item), 10);
+                inventory.insertItem("4x minecraft:oak_planks", false);
+            }
+        }
+    })
+
+}
+
+StartupEvents.registry("block", event => {
+    event.create("woodsaw")
+        .soundType(SoundType.STONE)
+        .blockEntity(entityInfo => {
+            entityInfo.inventory(3, 3);
+            entityInfo.rightClickOpensInventory();
+            entityInfo.serverTick(20, 0, entity => {
+                checkWoodsawRecipe(entity.inventory);
+            });
+        });
+})
