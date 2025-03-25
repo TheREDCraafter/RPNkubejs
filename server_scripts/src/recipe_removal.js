50b0ee0f0263180fbf0c0b78b35cc983be26cd98
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-03-25 09:25:40 +0100

    Added (experimental) bread in furnace recipe, removed normal one

--- server_scripts/src/recipe_removal.js
+++ server_scripts/src/recipe_removal.js
@@ -2,5 +2,9 @@ ServerEvents.recipes(event => {
     event.remove({
         input: "#minecraft:logs",
         output: "#minecraft:planks"
+    });
+    event.remove({
+        input: "minecraft:wheat",
+        output: "minecraft:bread"
     })
 })
