cc4ea0002a9bb3834ffaad2ba5c5a33835e423e4
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-04-02 17:20:57 +0200

    ADDed stripped log recipes, ADDed quark logs, FIX wheat -> bread recipe

--- server_scripts/src/recipe_adding.js
+++ server_scripts/src/recipe_adding.js
@@ -1,3 +1,3 @@
 ServerEvents.recipes(event => {
-    event.smelting("minecraft:bread", "10x minecraft:wheat", 0.1, 100);
+    
 })
--- startup_scripts/src/util.js
+++ startup_scripts/src/util.js
@@ -1,40 +1,56 @@
 const logList = [
-    'minecraft:oak_log',
-    'minecraft:spruce_log',
-    'minecraft:birch_log',
-    'minecraft:jungle_log',
-    'minecraft:acacia_log',
-    'minecraft:dark_oak_log',
-    'minecraft:mangrove_log',
-    'minecraft:cherry_log',
-    'minecraft:crimson_stem',
-    'minecraft:warped_stem',
+    // VANILLA
+    "minecraft:oak_log",
+    "minecraft:stripped_oak_log",
+    "minecraft:spruce_log",
+    "minecraft:stripped_spruce_log",
+    "minecraft:birch_log",
+    "minecraft:stripped_birch_log",
+    "minecraft:jungle_log",
+    "minecraft:stripped_jungle_log",
+    "minecraft:acacia_log",
+    "minecraft:stripped_acacia_log",
+    "minecraft:dark_oak_log",
+    "minecraft:stripped_dark_oak_log",
+    "minecraft:mangrove_log",
+    "minecraft:stripped_mangrove_log",
+    "minecraft:cherry_log",
+    "minecraft:stripped_cherry_log",
+    "minecraft:crimson_stem",
+    "minecraft:stripped_crimson_stem",
+    "minecraft:warped_stem",
+    "minecraft:stripped_warped_stem",
+    // MODDED
+    "quark:ancient_log",
+    "quark:stripped_ancient_log",
+    "quark:azalea_log",
+    "quark:stripped_azalea_log",
     // TODO: ...
 ]
 
 function getPlankFromLog(logId) {
     const logToPlankMap = {
-        'minecraft:oak_log': 'minecraft:oak_planks',
-        'minecraft:spruce_log': 'minecraft:spruce_planks',
-        'minecraft:birch_log': 'minecraft:birch_planks',
-        'minecraft:jungle_log': 'minecraft:jungle_planks',
-        'minecraft:acacia_log': 'minecraft:acacia_planks',
-        'minecraft:dark_oak_log': 'minecraft:dark_oak_planks',
-        'minecraft:mangrove_log': 'minecraft:mangrove_planks',
-        'minecraft:cherry_log': 'minecraft:cherry_planks',
-        'minecraft:crimson_stem': 'minecraft:crimson_planks',
-        'minecraft:warped_stem': 'minecraft:warped_planks',
+        "minecraft:oak_log": "minecraft:oak_planks",
+        "minecraft:spruce_log": "minecraft:spruce_planks",
+        "minecraft:birch_log": "minecraft:birch_planks",
+        "minecraft:jungle_log": "minecraft:jungle_planks",
+        "minecraft:acacia_log": "minecraft:acacia_planks",
+        "minecraft:dark_oak_log": "minecraft:dark_oak_planks",
+        "minecraft:mangrove_log": "minecraft:mangrove_planks",
+        "minecraft:cherry_log": "minecraft:cherry_planks",
+        "minecraft:crimson_stem": "minecraft:crimson_planks",
+        "minecraft:warped_stem": "minecraft:warped_planks",
     }
   
     if (logToPlankMap.hasOwnProperty(logId)) {
         return logToPlankMap[logId]
     }
   
-    const parts = logId.split(':')
+    const parts = logId.split(":")
     const modId = parts[0]
-    let woodType = parts[1].replace('stripped_', '').replace('_log', '').replace('_stem', '')
+    let woodType = parts[1].replace("stripped_", "").replace("_log", "").replace("_stem", "")
     
-    if (modId === 'minecraft') {
+    if (modId === "minecraft") {
         return `minecraft:${woodType}_planks`
     }
 
