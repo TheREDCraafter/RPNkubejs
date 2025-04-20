987b4d43608d5bdb1228829edbb6cbcc49b0a5d8
Fabian2611 <fabian.schwarz2010@gmail.com>
2025-04-20 17:00:04 +0200

    oops I am stupid, FIXed wrong ServerEvents.recipes()=>.shapeless usage

--- server_scripts/src/recipe_adding.js
+++ server_scripts/src/recipe_adding.js
@@ -1,15 +1,15 @@
 ServerEvents.recipes(event => {
     event.recipes.create.milling(Item.of("minecraft:clay_ball"), Item.of("minecraft:cobblestone")).id("create:milling/clay_ball_from_cobblestone")
     event.shapeless(
-        "kubejs:hydrochloric_acid",
+        Item.of("kubejs:hydrochloric_acid"),
         ["refurbished_furniture:sea_salt", "meds_and_herbs:bottled_water", "kubejs:sulfuric_acid"]
     )
     event.shapeless(
-        "kubejs:sulfuric_acid",
+        Item.of("kubejs:sulfuric_acid"),
         ["meds_and_herbs:bottled_water", "minecraft:gunpowder", "minecraft:lava_bucket"]
     )
     event.shapeless(
-        "kubejs:cocaine",
+        Item.of("kubejs:cocaine"),
         ["kubejs:hydrochloric_acid", "kubejs:cocaine_leaf", "kubejs:cocaine_leaf", "kubejs:cocaine_leaf"]
     )
 })
